e1a5a59a61b20e9f8397736022bae8b3ea07bd24
GeorgeAlbu <george.albu1@gmail.com>
2023-01-16 11:17:38 +0200

    ga added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from 'next/app'
 import React, { useEffect, useState } from 'react'
 import { ThemeProvider } from 'next-themes'
 import { Layout, SiteLayout } from '../components'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps, router }: AppProps) {
   if (
@@ -11,20 +12,46 @@ function MyApp({ Component, pageProps, router }: AppProps) {
     router.pathname.startsWith('/category/')
   ) {
     return (
-      <ThemeProvider attribute="class">
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </ThemeProvider>
+      <>
+        <Script
+          strategy="afterInteractive"
+          src="https://www.googletagmanager.com/gtag/js?id=G-BNGPR7X9DX"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-BNGPR7X9DX')`}
+        </Script>
+        <ThemeProvider attribute="class">
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </ThemeProvider>
+      </>
     )
   }
 
   return (
-    <ThemeProvider attribute="class">
-      <SiteLayout>
-        <Component {...pageProps} />
-      </SiteLayout>
-    </ThemeProvider>
+    <>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-BNGPR7X9DX"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-BNGPR7X9DX')`}
+      </Script>
+      <ThemeProvider attribute="class">
+        <SiteLayout>
+          <Component {...pageProps} />
+        </SiteLayout>
+      </ThemeProvider>
+    </>
   )
 }
 
